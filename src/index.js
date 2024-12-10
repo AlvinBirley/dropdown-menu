8016e63c4f9ff2aab376109b5ef03e5b3b5a4237
AlvinBirley <alvinbirley@gmail.com>
2024-12-10 17:51:04 -0600

    Add hover effect to hamburer icon

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import './styles.css';
 
+const getBurger = document.querySelector(".hamburger")
+
+const lilBurgers = document.getElementsByClassName("lil-burger-divs");
+
+getBurger.addEventListener('mouseover', function() {
+   
+  
+
+    // Loop through all lilBurgers and change the background color
+    for (let i = 0; i < lilBurgers.length; i++) {
+      lilBurgers[i].style.backgroundColor = "white";
+    }
+  });
