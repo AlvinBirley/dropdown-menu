725f32843a77f88375d15fc3576c8fcf95645839
AlvinBirley <alvinbirley@gmail.com>
2024-12-11 01:11:05 -0600

    Add setTimeout to event listeners so dropdown-menu functions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,52 @@
 import './styles.css';
 
-const getBurger = document.querySelector(".hamburger")
-
+const getBurger = document.querySelector(".hamburger");
 const lilBurgers = document.getElementsByClassName("lil-burger-divs");
+const getDropDown = document.querySelector(".links");
 
+let hoveringHamburger = false;
+let hoveringLinks = false;
+
+// Show the dropdown when hovering over the hamburger
 getBurger.addEventListener('mouseover', function() {
-   
-  
-
-    // Loop through all lilBurgers and change the background color
+    hoveringHamburger = true;
+    // Change background color of lil burger divs
     for (let i = 0; i < lilBurgers.length; i++) {
-      lilBurgers[i].style.backgroundColor = "white";
+        lilBurgers[i].style.backgroundColor = "white";
     }
-  });
+    getDropDown.style.display = 'block';
+});
+
+// Function to hide dropdown
+function dropDownGoUp() {
+    for (let i = 0; i < lilBurgers.length; i++) {
+        lilBurgers[i].style.backgroundColor = "black";
+    }
+    getDropDown.style.display = 'none';
+}
+
+
+getBurger.addEventListener('mouseleave', function() {
+    hoveringHamburger = false;
+   
+    setTimeout(() => {
+        if (!hoveringLinks) {
+            dropDownGoUp();
+        }
+    }, 100); 
+});
+
+
+getDropDown.addEventListener('mouseover', function() {
+    hoveringLinks = true;
+});
+
+getDropDown.addEventListener('mouseleave', function() {
+    hoveringLinks = false;
+   
+    setTimeout(() => {
+        if (!hoveringHamburger) {
+            dropDownGoUp();
+        }
+    }, 100); 
+});
